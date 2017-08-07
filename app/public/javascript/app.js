d754df49c323936a64f0b443a37e4ace6486c8f3
YETC7 <tyler_726@live.com>
2017-08-06 21:26:14 -0700

    add select2

--- app/public/javascript/app.js
+++ app/public/javascript/app.js
@@ -1,75 +1,58 @@
-$("#submit").on("click",function(event) {
-  event.preventDefault();
+$(document).ready(function() {
 
-  var currentURL = window.location.origin;
+  $('select').select2({});
 
-  var nameVal = $("#name").val().trim();
-  var photoVal = $("#photo").val().trim();
+  // $(".select2-selection__rendered").css("width","100px");
 
-  if (nameVal === "" || photoVal === "") {
-    $("#error").html("*All fields are required*");
-    $("#error").show();
-    return;
-  }
+  $("#submit").on("click",function(event) {
+    event.preventDefault();
 
-  $("#error").hide();
+    var currentURL = window.location.origin;
 
-  var newUser = {
-    "name":nameVal,
-    "photo":photoVal,
-    "scores":[
-      $("#ques1").val(),
-      $("#ques2").val(),
-      $("#ques3").val(),
-      $("#ques4").val(),
-      $("#ques5").val(),
-      $("#ques6").val(),
-      $("#ques7").val(),
-      $("#ques8").val(),
-      $("#ques9").val(),
-      $("#ques10").val()
-    ]
-  };
+    var nameVal = $("#name").val().trim();
+    var photoVal = $("#photo").val().trim();
 
-  $("#name").val("");
-  $("#photo").val("");
+    if (nameVal === "" || photoVal === "" || 
+      $("#ques1").val() === null || $("#ques2").val() === null ||
+      $("#ques3").val() === null || $("#ques4").val() === null ||
+      $("#ques5").val() === null || $("#ques6").val() === null ||
+      $("#ques7").val() === null || $("#ques8").val() === null ||
+      $("#ques9").val() === null || $("#ques10").val() === null) 
+    {
+        $("#error").html("*All fields are required*");
+        $("#error").show();
+        return;
+    }
 
-  $.post(currentURL + "/api/friends", newUser, function(data) {
-    console.log(data);
-    $("#newFriendName").text(data.name);
-    $("#newFriendPhoto").attr("src",data.photo);
-    $("#friendFound").modal("toggle");
+    $("#error").hide();
+
+    var newUser = {
+      "name":nameVal,
+      "photo":photoVal,
+      "scores":[
+        $("#ques1").val(),
+        $("#ques2").val(),
+        $("#ques3").val(),
+        $("#ques4").val(),
+        $("#ques5").val(),
+        $("#ques6").val(),
+        $("#ques7").val(),
+        $("#ques8").val(),
+        $("#ques9").val(),
+        $("#ques10").val()
+      ]
+    };
+
+    $("#name").val("");
+    $("#photo").val("");
+
+    $.post(currentURL + "/api/friends", newUser, function(data) {
+      console.log(data);
+      $("#newFriendName").text(data.name);
+      $("#newFriendPhoto").attr("src",data.photo);
+      $("#friendFound").modal("toggle");
+    });
+
   });
-
+  
 });
-
-// function findClosestMatch(user,Users) {
-//   var smallestDiff = 999;
-//   var closest;
-//   for(var i = 0; i<Users.length; i++) {
-//     if (user !== Users[i]) {
-//       var newDiff = totalDifference(user,Users[i]);
-//       if (newDiff < smallestDiff) {
-//         smallestDiff = newDiff;
-//         closest = Users[i];
-//       }
-//     }
-//   }
-//   console.log(closest);
-// }
-
-// function totalDifference(userA,userB) {
-//   var totalDiff = 0;
-//   for(var i = 0; i<userA.scores.length; i++) {
-//     totalDiff+=absoluteDifference(userA.scores[i],userB.scores[i]);
-//   }
-//   return totalDiff;
-// }
-
-// function absoluteDifference(x,y) {
-//   if (x > y){
-//     return x-y;
-//   }
-
-//   return y-x;
-// }
